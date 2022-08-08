2130b7cb3d8b939bf58e6032ab8d697284b598a6
lucas <gepeoverload@gmail.com>
2022-08-08 09:43:20 +0200

    added logic to get data from the server both on form submit and page load

--- friendlist/src/app/form/form.component.ts
+++ friendlist/src/app/form/form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { observable } from 'rxjs';
 import { Friend } from '../models/friend';
 import { AddFriendService } from '../services/add-friend.service';
 
@@ -11,10 +10,14 @@ import { AddFriendService } from '../services/add-friend.service';
 export class FormComponent implements OnInit {
   languages: Array<string> = ['html', 'css', 'javascript', 'php'];
   friendModel = new Friend('', '', '', '', '');
+  allFriends: any;
+  allFriendsUrl: string = 'http://localhost:6969/allFriends';
 
   constructor(private addFriendService: AddFriendService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getFriends(this.allFriendsUrl);
+  }
 
   submitForm() {
     const observable = this.addFriendService.addFriend(this.friendModel);
@@ -24,11 +27,26 @@ export class FormComponent implements OnInit {
       next: (data) => {
         console.log(data);
         console.log('success!');
+        this.getFriends(this.allFriendsUrl);
       },
       error: (error) => {
         console.log(error);
         console.log('failed');
       },
     });
+  }
+  public async getFriends(url: string): Promise<any> {
+    try {
+      const data = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      this.allFriends = await data.json();
+      console.log(this.allFriends);
+    } catch (err) {
+      console.log('An error has ocurred: ' + err);
+    }
   }
 }
